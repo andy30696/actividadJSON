aabb967755fea9ee7a2f47472f663e5a7fcba17d
andy30696 <andy30696@hotmail.com>
2023-08-14 11:29:06 -0300

    script

---
+++ script.js
@@ -0,0 +1,13 @@
+const DATA_URL = "series.json";
+
+const container = document.getElementById("seriesContainer");
+
+function showData(dataArray) {
+    for (const item of dataArray) {
+        seriesContainer.innerHTML += `<p> ${item.portada} ${item.titulo} </p>`;
+    }
+}
+fetch(DATA_URL)
+  .then((response) => response.json())
+  .then((data) => showData(series.lista_series))
+  .catch((error) => console.error("Error al obtener los datos:", error));
